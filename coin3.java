e76245c6b2f04061d6b2daa8d6691ffd3c163a8a
snowman-1115 <109066005+snowman-1115@users.noreply.github.com>
2022-07-11 18:45:04 +0900

    Victory message added

---
+++ coin3.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class coin3{
+    public static void main(String[] args){
+        System.out.println("Tossing a coin...");
+
+	int head = 0;
+        int tail = 0;
+        for(int i = 1; i <= 3; i++){
+            Random coin = new Random();
+            int num = coin.nextInt(5);
+            if(num%2 == 0){
+                System.out.println("Round " + i + ": Heads");
+                head++;
+            }       
+            else if(num%2 == 1){
+                System.out.println("Round " + i + ": Tails"); 
+		tail++;                 
+            }
+        }   
+	System.out.println("Heads: " + head + ", Tails: " + tail);
+	if(head > tail)	System.out.println("You won");
+	else System.out.println("You lost");
+    }   
+}
